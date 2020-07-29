08e2393d3aabdb4812399f1c2e8553f64c39c037
Chamith Nimmitha <chamith123.cn@gmail,com>
2020-07-29 19:01:35 +0530

    Modify Assignment 05

--- Assignment-05/q4/Bank.scala
+++ Assignment-05/q4/Bank.scala
@@ -21,5 +21,5 @@ object Bank {
 //  def totalBalance(bank: List[Account]) = bank.reduceLeft((x,y) => {new Account("9999999",9999999,x.balance + y.balance); })
   def totalBalance =  println(s"BANK => Total balance :  ${accList.foldLeft(0.0)((x,y) => x + y.balance)}")
   
-  def applyInterest = {accList.foreach( x => {if (x.balance>0) x.balance*=1.05 else x.balance * 0.9}); println("BANK => Interest applied..")}
+  def applyInterest = {accList.foreach( x => {if (x.balance>0) x.balance*=1.05 else x.balance *= 1.1}); println("BANK => Interest applied..")}
 }
